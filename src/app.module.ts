7ff546ad26518e98048b4d0fb365c42d09ba2453
anaikarsaad <anaikarsaad@gmail.com>
2023-09-06 19:24:21 -0400

    added swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule} from '@nestjs/mongoose';
 
 @Module({
   imports: [
